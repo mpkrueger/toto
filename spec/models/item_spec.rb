b2ae324f68d15a0b7f6aa0a5afbd5949a8227fb2
mpkrueger <mpkrueger@gmail.com>
2015-02-28 10:25:21 -0800

    Updated item model spec to test single method

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Item do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:item) { FactoryGirl.create(:item) }
+
+  describe "mark_complete" do
+    
+    it "marks the item completed" do
+      expect{ item.mark_complete }
+        .to change{ item.completed }
+        .to true
+    end
+  end
+
 end
