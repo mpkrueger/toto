0aec72ce56bded28d03e4da5194e46d00d951ccb
mpkrueger <mpkrueger@gmail.com>
2015-02-28 10:00:11 -0800

    Updated and got passing all items controller specs

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -2,13 +2,34 @@ class Api::ItemsController < ApplicationController
   def create
     list = List.find(item_params[:list_id])
     item = Item.new(item_params)
+    user = User.find(user_params[:id])
+
+    if user.authenticate?(user_params[:password]) == false
+      head :bad_request and return
+    end
 
     if item.save
       render json: item
     end
   end
 
+  def index
+    list = List.find(params[:list_id])
+    user = User.find(user_params[:id])
+    items = list.items
+
+    if user.authenticate?(user_params[:password]) == false
+      head :bad_request and return
+    end
+
+    render json: items, only: [:list_id, :description]
+  end
+
   private
+
+  def user_params
+    params.require(:user).permit(:id, :username, :password)
+  end
 
   def item_params
     params.require(:item).permit(:list_id, :description)
--- spec/controllers/api/items_controller_spec.rb
+++ spec/controllers/api/items_controller_spec.rb
@@ -3,13 +3,14 @@ require 'spec_helper'
 describe Api::ItemsController do 
   before do
     @list = List.create(name: "Santa's List", user_id: 3)
+    @user = User.create(username: "Santa", password: "rudolph")
   end
 
   describe "create" do
     context "with correct user's password" do
 
       it "takes an item name and creates it if it doesn't exit" do
-        params = { item: { list_id: @list.id, description: "Toy truck" } }
+        params = { user: { id: @user.id, username: @user.username, password: @user.password }, item: { list_id: @list.id, description: "Toy truck" } }
 
         expect{ post :create, params }
           .to change{ Item.count }
@@ -18,19 +19,48 @@ describe Api::ItemsController do
     end
 
     context "without correct user's password" do
-      xit "returns an error"
+
+      it "returns an error" do
+        params = { user: { id: @user.id, username: @user.username, password: "wrong" }, item: { list_id: @list.id, description: "doll" } }
+
+        post :create, params
+        expect(response.status).to eq 400
+      end
     end
   end
 
   describe "index" do
+    before do
+      (1..3).each{ |n| Item.create( list_id: @list.id, description: "Robot#{n}") }
+    end
+
     context "with correct user's password" do
 
-      xit "returns all items associated with the list"
+      it "returns all items associated with the list" do
+        params = { user: { id: @user.id, username: @user.username, password: @user.password }, list_id: @list.id }
+
+        get :index, params
+        JSON.parse(response.body).should ==
+          { 'items' =>
+            [
+              { 'list_id' => @list.id, 'description' => 'Robot1'},
+              { 'list_id' => @list.id, 'description' => 'Robot2'},
+              { 'list_id' => @list.id, 'description' => 'Robot3'}
+            ]
+          }
+
+      end
+
     end
 
     context "without correct user's password" do
 
-      xit "returns an error"
+      it "returns an error" do
+        params = { user: { id: @user.id, username: @user.username, password: "wrong" }, list_id: @list.id }
+
+        get :index, params
+        expect(response.status).to eq 400
+      end
     end
 
   end
